4a2694bbdec5455a2663d0d17a691b14c7a3131d
Matteo Sacchi <matteoseik@hotmail.it>
2022-05-16 16:39:53 +0200

    fix svg e fade

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,4 +1,9 @@
 import Marquee3k from 'marquee3000';
+import { gsap } from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
 Marquee3k.init()
 
@@ -22,4 +27,23 @@ window.onclick = function(event) {
   }
 }
 
-console.log("ciao")
+
+
+// // fade up
+const fadeUp = gsap.utils.toArray("[fade]");
+fadeUp.forEach((el, i) => {
+  const anim = gsap.fromTo(el, {autoAlpha: 0, y:0}, {duration: 1.5, autoAlpha: 1});
+  ScrollTrigger.create({
+    trigger: el,
+    animation: anim,
+    toggleActions: 'play none none none',
+    once: true,
+  });
+});
+
+
+let h = document.querySelector(".hero_section").offsetHeight;
+let arrow = document.querySelector(".scroll");
+arrow.addEventListener("click", function(){
+  gsap.to(window, {duration: 1, scrollTo: h});
+});
